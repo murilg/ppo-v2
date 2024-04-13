4b5342774131de6aa7c307be486c721fc015937a
Ilgiz <ktoto32523@ya.ru>
2024-04-13 14:51:08 +0500

    Ещё

--- src/main/java/com/example/demo/Demo2Application.java
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -8,10 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class Demo2Application {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int num = in.nextInt();
+
+        System.out.printf("Your number: %d \n", num);
+        in.close();
+
         SpringApplication.run(Demo2Application.class, args);
     }
 
